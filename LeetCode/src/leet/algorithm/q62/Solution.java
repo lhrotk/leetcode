740f5900e646147522c0e735c9ebd427947b3aad
hongru <lhrotk@gmail.com>
2017-10-13 12:14:08 -0700

    q62

---
+++ LeetCode/src/leet/algorithm/q62/Solution.java
@@ -0,0 +1,26 @@
+package leet.algorithm.q62;
+
+public class Solution {
+	public int uniquePaths(int m, int n) {
+		if(m<=1 || n<=1)
+			return 1;
+		if(m>n) {
+			int temp = m;
+			m = n; 
+			n = temp;
+		}
+        long divident = 1;
+        for(int i=0; i<=m-2; i++) {
+        	divident *= m+n-2-i;
+        	divident /= i+1;
+        }
+        return (int)(divident);
+    }
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Solution sol = new Solution();
+		System.out.println(sol.uniquePaths(36, 7));
+	}
+
+}
